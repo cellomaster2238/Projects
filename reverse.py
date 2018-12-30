1e4992e61446a4af74ae76f50fbcea001c84e42e
cellomaster2238 <46263486+cellomaster2238@users.noreply.github.com>
2018-12-30 17:01:04 -0600

    Reverses user input string

---
+++ reverse.py
@@ -0,0 +1,8 @@
+# Reverse a String - Enter a string and the program
+# will reverse it and print it out.
+
+def reverse():
+    string = input('Write something: ')
+    print(string[ : : -1])
+
+reverse()
